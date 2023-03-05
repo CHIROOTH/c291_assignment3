67fe4a505a89e10c0bc58934bfde6a3e01dc32a9
CHIROOTH <90502325+CHIROOTH@users.noreply.github.com>
2023-03-05 02:13:01 -0700

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -2,7 +2,12 @@ SELECT Oi1.order_id
 FROM Customers C, Orders O, Order_items Oi1
 WHERE C.customer_id = O.customer_id
 AND O.order_id = Oi1.order_id
-AND C.customer_postal_code = (?)
+AND C.customer_postal_code = (
+    SELECT C2.customer_postal_code
+    FROM Customers C2 LIMIT 1
+    ORDER BY RANDOM()
+
+)
 GROUP BY Oi1.order_id
 HAVING COUNT(Oi1.order_item_id) > 1
--- ? - is some postal code which is given 
+;
