697ffce7c24fd4fee3ce4dafd1ca8058229aa66f
CHIROOTH <90502325+CHIROOTH@users.noreply.github.com>
2023-03-05 19:00:26 -0700

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,22 +1,26 @@
--- CREATE VIEW OrderSize (orderid, size)
--- AS SELECT O.order_id, COUNT(DISTINCT Oi1.order_item_id)
--- FROM Orders O, Order_items Oi1
--- WHERE O.order_id = Oi1.order_id
--- GROUP BY Oi1.order_id
--- ;
+CREATE VIEW OrderSize AS
+SELECT order_id AS oid, COUNT(*) AS size
+FROM Order_items
+GROUP BY order_id;
 
-SELECT COUNT(DISTINCT Oi1.order_id)
-FROM Customers C, Orders O, Order_items Oi1
-WHERE C.customer_id = O.customer_id
-AND O.order_id = Oi1.order_id
-AND C.customer_postal_code = (
-    SELECT C2.customer_postal_code
-    FROM Customers C2
-    GROUP BY C2.customer_postal_code
-    ORDER BY RANDOM()
+SELECT COUNT(DISTINCT Orders.order_id)
+FROM Orders
+WHERE Orders.customer_id IN (
+    SELECT Customers.customer_id
+    FROM Customers
+    WHERE Customers.customer_postal_code = [random postal code]
 )
-AND (
-SELECT COUNT(Os.size) FROM OrderSize Os) > (SELECT AVG(Os.size) FROM OrderSize Os )
-GROUP BY Oi1.order_id
-HAVING COUNT(Oi1.order_item_id) > 1
-;
+AND Orders.order_id IN (
+    SELECT Order_items.order_id
+    FROM Order_items
+    GROUP BY Order_items.order_id
+    HAVING COUNT(DISTINCT Order_items.order_item_id) > 1
+)
+AND Orders.order_id IN (
+    SELECT OrderSize.oid
+    FROM OrderSize
+    WHERE OrderSize.size > (
+        SELECT AVG(size)
+        FROM OrderSize
+    )
+);
